0fbb128c094ebc0a10ce9be0c1433a7f4221b939
AAABBBCCC000 <k023g1043@g.neec.ac.jp>
2024-06-04 16:41:19 +0900

    update

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,12 +1,61 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
+#include "cmath"
 
 const char kWindowTitle[] = "GC2C_14_リ_ブンタツ";
+
+Matrix4x4 MakeOrthographicMatrix(
+    float left, float top, float right, float bottom, float nearClip, float farClip) {
+	return {
+	    2.0f / (right - left), 0.0f, 0.0f, 0.0f,
+	    0.0f, 2.0f / (top - bottom), 0.0f, 0.0f,
+	    0.0f, 0.0f, 1.0f / (farClip - nearClip), 0.0f,
+	    (left + right) / (left - right), (top + bottom) / (bottom - top), nearClip / (nearClip - farClip), 1.0f,
+	};
+}
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	float cotHalfFovV = 1.0f / std::tan(fovY / 2.0f);
+
+	return {
+	    (cotHalfFovV / aspectRatio), 0.0f, 0.0f, 0.0f,
+	    0.0f, cotHalfFovV, 0.0f, 0.0f,
+	    0.0f, 0.0f, farClip / (farClip - nearClip), 1.0f,
+		0.0f, 0.0f, -(nearClip*farClip) / (farClip - nearClip), 0.0f
+	};
+}
+
+Matrix4x4 MakeViewportMatrix(
+    float left, float top, float width, float height, float minDepth, float maxDepth) {
+	return {
+	    width / 2.0f, 0.0f, 0.0f, 0.0f,
+	    0.0f, - height / 2.0f, 0.0f, 0.0f,
+	    0.0f, 0.0f, maxDepth - minDepth, 0.0f,
+	    left + width / 2.0f, top + height / 2.0f, minDepth, 1.0f,
+	};
+}
+
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, label);
+	for (int row = 0; row < 4; row++) {
+		for (int column = 0; column < 4; column++) {
+			Novice::ScreenPrintf(
+			    x + column * kColumnWidth, y + row * kRowHeight + kRowHeight, "%6.02f",
+			    matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -21,21 +70,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		
+		///
 		/// ↓更新処理ここから
-		
+		///
 
-		
+		Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.f, 160.f, 200.0f, 300.0f, 0.0f, 1000.0f);
+
+		Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
+		///
 		/// ↑更新処理ここまで
-		
+		///
 
-		
+		///
 		/// ↓描画処理ここから
-		
+		///
 
-		
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
+
+		///
 		/// ↑描画処理ここまで
-		
+		///
 
 		// フレームの終了
 		Novice::EndFrame();
